f8709b3fc6f93f7b85245ca93cad86242a2b4bef
shida17-fullstack <abogadanbutera@gmail.com>
2024-09-30 22:12:21 -0300

    Actualiz variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           console.log('Conectando a la base de datos en producción');
           return {
             type: 'postgres', // PostgreSQL para producción
-            host: configService.get<string>('DATABASE_HOST') || 'dpg-crtgdd3v2p9s73ctjhq0-a',
-            port: parseInt(configService.get<string>('DATABASE_PORT'), 10) || 5432,
-            username: configService.get<string>('DATABASE_USER') || 'root',
-            password: configService.get<string>('DATABASE_PASSWORD') || 'nWT4Aik6lxi3yEHb1RzG8zj2v6VtSWEL',
-            database: configService.get<string>('DATABASE_NAME') || 'biblioteca_3kah',
+            url: configService.get<string>('DATABASE_URL') || 'postgresql://root:nWT4Aik6lxi3yEHb1RzG8zj2v6VtSWEL@dpg-crtgdd3v2p9s73ctjhq0-a/biblioteca_3kah',
             entities: [__dirname + '/**/*.entity{.ts,.js}'], // Archivos de entidades
             synchronize: false, // No sincronizar en producción
             logging: true,
