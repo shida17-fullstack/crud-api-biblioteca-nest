079055dad5f83d984445514be377047002c9f6c9
shida17-fullstack <abogadanbutera@gmail.com>
2024-10-01 17:30:16 -0300

    Actualiza archivos ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,40 +6,48 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Asegura que las variables de entorno estén disponibles globalmente
+      isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
         const isProduction = process.env.NODE_ENV === 'production';
+        const sslEnabled = process.env.SSL === 'true';
 
         if (isProduction) {
           console.log('Conectando a la base de datos en producción');
           return {
-            type: 'postgres', // PostgreSQL para producción
-            url: process.env.DATABASE_URL || 'postgresql://root:nWT4Aik6lxi3yEHb1RzG8zj2v6VtSWEL@dpg-crtgdd3v2p9s73ctjhq0-a.oregon-postgres.render.com/biblioteca_3kah',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'], // Archivos de entidades
+            type: 'postgres',
+            host: process.env.DATABASE_HOST || 'dpg-crtgdd3v2p9s73ctjhq0-a.oregon-postgres.render.com',
+            port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+            username: process.env.DATABASE_USERNAME || 'root',
+            password: process.env.DATABASE_PASSWORD || 'nWT4Aik6lxi3yEHb1RzG8zj2v6VtSWEL',
+            database: process.env.DATABASE_NAME || 'biblioteca_3kah',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
             synchronize: false, // No sincronizar en producción
             logging: true,
+            ssl: sslEnabled
+              ? { rejectUnauthorized: false } // SSL activado con verificación deshabilitada
+              : false, // No usar SSL si no está habilitado
           };
         } else {
           console.log('Conectando a la base de datos en desarrollo');
           return {
-            type: 'mysql', // MySQL para desarrollo
+            type: 'mysql',
             host: process.env.DATABASE_HOST || 'localhost',
             port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
             username: process.env.DATABASE_USER || 'root',
             password: process.env.DATABASE_PASSWORD || 'shida17',
             database: process.env.DATABASE_NAME || 'biblioteca',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'], // Archivos de entidades
-            synchronize: true, // Sincronizar automáticamente en desarrollo
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true, // Sincronización automática en desarrollo
             logging: true,
           };
         }
       },
       inject: [ConfigService],
     }),
-    V1Module, // Incluye otros módulos necesarios
+    V1Module,
   ],
 })
 export class AppModule {}
