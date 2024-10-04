958cae110b602370fc1d5e950c1c4d64763d535e
shida17-fullstack <abogadanbutera@gmail.com>
2024-10-04 18:10:32 -0300

    modifica archivos

--- src/v1/libros/libros.controller.ts
+++ src/v1/libros/libros.controller.ts
@@ -18,7 +18,7 @@ import { Libro } from '@libros/libro.entity';
 import { CreateLibroDTO } from '@libros/dto/create-libro.dto';
 import { UpdateLibroDTO } from '@libros/dto/update-libro.dto';
 import { JwtAuthGuard } from '@auth/jwt-auth.guard';
-import { Request } from 'express';
+import { Request } '@nestjs/common';
 import { JwtPayload } from '@auth/jwt-payload.interface';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
