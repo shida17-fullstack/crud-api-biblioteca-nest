db982a25deb6bf9704d91dc5a3d0a2d197e9952f
shida17-fullstack <abogadanbutera@gmail.com>
2024-10-04 12:18:22 -0300

    Modifica app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,29 +21,29 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
           console.log('Conectando a la base de datos en producción');
           return {
             type: 'postgres',
-            host: process.env.DATABASE_HOST || 'dpg-crtgdd3v2p9s73ctjhq0-a.oregon-postgres.render.com',
-            port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-            username: process.env.DATABASE_USERNAME || 'root',
-            password: process.env.DATABASE_PASSWORD || 'nWT4Aik6lxi3yEHb1RzG8zj2v6VtSWEL',
-            database: process.env.DATABASE_NAME || 'biblioteca_3kah',
+            host: configService.get('DATABASE_HOST'),
+            port: parseInt(configService.get('DATABASE_PORT', 10)),
+            username: configService.get('DATABASE_USERNAME'),
+            password: configService.get('DATABASE_PASSWORD'),
+            database: configService.get('DATABASE_NAME'),
             entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false, // No sincronizar en producción
+            synchronize: false,
             logging: true,
             ssl: sslEnabled
-              ? { rejectUnauthorized: false } // Deshabilitar verificación de certificado si es necesario
-              : false, // No usar SSL si no está habilitado
+              ? { rejectUnauthorized: false }
+              : false,
           };
         } else {
           console.log('Conectando a la base de datos en desarrollo');
           return {
             type: 'mysql',
-            host: process.env.DATABASE_HOST || 'localhost',
-            port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-            username: process.env.DATABASE_USER || 'root',
-            password: process.env.DATABASE_PASSWORD || 'shida17',
-            database: process.env.DATABASE_NAME || 'biblioteca',
+            host: configService.get('DATABASE_HOST', 'localhost'),
+            port: parseInt(configService.get('DATABASE_PORT', 10)) || 3306,
+            username: configService.get('DATABASE_USER', 'root'),
+            password: configService.get('DATABASE_PASSWORD', 'shida17'),
+            database: configService.get('DATABASE_NAME', 'biblioteca'),
             entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true, // Sincronización automática en desarrollo
+            synchronize: true,
             logging: true,
           };
         }
@@ -54,3 +54,4 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   ],
 })
 export class AppModule {}
+
