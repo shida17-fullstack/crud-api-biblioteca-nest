a8ced0c5aa548c6fad0b4c2ac262f4223a47ae56
shida17-fullstack <abogadanbutera@gmail.com>
2024-10-05 15:55:53 -0300

    Actualiza app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,41 +6,51 @@ import { V1Module } from '@v1/v1.module';
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async () => {
-        const isProduction = process.env.NODE_ENV === 'production';
-        const sslEnabled = process.env.SSL === 'true';
+        try {
+          const isProduction = process.env.NODE_ENV === 'production';
+          const sslEnabled = process.env.SSL === 'true';
 
-        console.log('Entorno de ejecución:', process.env.NODE_ENV);
-        console.log('SSL Enabled:', sslEnabled);
+          console.log('Entorno de ejecución:', process.env.NODE_ENV);
+          console.log('SSL Enabled:', sslEnabled);
 
-        if (isProduction) {
-          console.log('Conectando a la base de datos en producción');
-          return {
-            type: 'postgres',
-            host: process.env.DATABASE_HOST,
-            port: parseInt(process.env.DATABASE_PORT, 10), // Convertir puerto a número
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME,
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false,
-            logging: true,
-            ssl: sslEnabled
-              ? { rejectUnauthorized: false }
-              : false,
-          };
-        } else {
-          console.log('Conectando a la base de datos en desarrollo');
-          return {
-            type: 'mysql',
-            host: process.env.DATABASE_HOST || 'localhost',
-            port: parseInt(process.env.DATABASE_PORT, 10) || 3306, // Convertir puerto a número
-            username: process.env.DATABASE_USER || 'root',
-            password: process.env.DATABASE_PASSWORD || 'shida17',
-            database: process.env.DATABASE_NAME || 'biblioteca',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,
-            logging: true,
-          };
+          if (isProduction) {
+            console.log('Conectando a la base de datos en producción');
+            return {
+              type: 'postgres',
+              host: process.env.DATABASE_HOST,
+              port: parseInt(process.env.DATABASE_PORT, 10), // Convertir puerto a número
+              username: process.env.DATABASE_USERNAME,
+              password: process.env.DATABASE_PASSWORD,
+              database: process.env.DATABASE_NAME,
+              entities: [__dirname + '/**/*.entity{.ts,.js}'],
+              synchronize: false,
+              logging: true,
+              ssl: sslEnabled
+                ? { rejectUnauthorized: false }
+                : false,
+              extra: {
+                application_name: 'crud-api-biblioteca-nest', // Nombre de la aplicación
+              },
+            };
+          } else {
+            console.log('Conectando a la base de datos en desarrollo');
+            return {
+              type: 'mysql',
+              host: process.env.DATABASE_HOST || 'localhost',
+              port: parseInt(process.env.DATABASE_PORT, 10) || 3306, // Convertir puerto a número
+              username: process.env.DATABASE_USER || 'root',
+              password: process.env.DATABASE_PASSWORD || 'shida17',
+              database: process.env.DATABASE_NAME || 'biblioteca',
+              entities: [__dirname + '/**/*.entity{.ts,.js}'],
+              synchronize: true,
+              logging: true,
+            };
+          }
+        } catch (error) {
+          // Captura y muestra el error
+          console.error('Error al configurar la conexión a la base de datos:', error.message);
+          console.error('Detalles completos del error:', error);
+          throw new Error('No se pudo establecer la conexión con la base de datos.');
         }
       },
       inject: [],
@@ -49,4 +59,3 @@ import { V1Module } from '@v1/v1.module';
   ],
 })
 export class AppModule {}
-
